42f7927b3d0d954661dd9e31c0ef4768d144997d
mahdi <mahdi.mo8117@gmail.com>
2026-02-01 16:28:20 +0330

    add active senario for mobile navbar

--- src/components/layout/NavMenu.tsx
+++ src/components/layout/NavMenu.tsx
@@ -1,3 +1,4 @@
+"use client"
 import React from 'react';
 import Link from 'next/link';
 import UserIco from '../module/UserIco';
@@ -5,38 +6,49 @@ import CpuIco from '../module/CpuIco';
 import FolderIco from '../module/FolderIco';
 import MailIco from '../module/MailIco';
 import HomeIco from '../module/HomeIco';
+import { usePathname } from 'next/navigation';
 
 interface NavItems {
     label: string;
     href: string;
     icon: React.ReactNode;
+    iconComponent: React.ComponentType<{ color?: string }>;
 }
 const MenuItems: NavItems[] = [
-    { label: "خانه", href: "/", icon: <HomeIco /> },
-    { label: "درباره من", href: "/about-me", icon: <UserIco /> },
-    { label: "مهارت ها", href: "/skills", icon: <CpuIco/> },
-    { label: "پروژه ها", href: "/projects", icon: <FolderIco /> },
-    { label: "تماس با من", href: "/contact-me", icon: <MailIco /> },
+    { label: "خانه", href: "/", icon: <HomeIco />, iconComponent: HomeIco },
+    { label: "درباره من", href: "/about-me", icon: <UserIco />, iconComponent: UserIco },
+    { label: "مهارت ها", href: "/skills", icon: <CpuIco />, iconComponent: CpuIco },
+    { label: "پروژه ها", href: "/projects", icon: <FolderIco />, iconComponent: FolderIco },
+    { label: "تماس با من", href: "/contact-me", icon: <MailIco />, iconComponent: MailIco },
 ]
 function NavMenu() {
+    const pathname = usePathname();
+    const isActive = (href: string) => {
+        return pathname === href;
+    }
     return (
         <>
-
             <header className='hidden md:flex'>
                 <nav className="nav-menu ">
                     <ul className="flex justify-between text-white font-bold ">
                         {MenuItems.map((item) => (
-                            <li key={item.label} className='cursor-pointer hover:text-[#A855F7]  hover:scale-[1.2] transition-all ease-in-out duration-300'><Link href={item.href}>{item.label}</Link></li>
+                            <li key={item.label} className={`${isActive(item.href) ? 'text-[#A855F7] scale-[1.2]' : ''} cursor-pointer hover:text-[#A855F7]  hover:scale-[1.2] transition-all ease-in-out duration-300`}><Link href={item.href}>{item.label}</Link></li>
                         ))}
                     </ul>
                 </nav>
             </header>
-            <nav className='md:hidden fixed bottom-5 left-0 w-[90%] translate-x-[5%]  text-white z-50 inset-shadow-[3px_3px_6px_rgba(255,255,255,0.2),-3px_-3px_6px_rgba(255,255,255,0.2)] items-center rounded-2xl backdrop-blur-xl' style={{paddingBlock:"1rem"}}>
+            <nav className='md:hidden fixed bottom-5 left-0 w-[90%] translate-x-[5%]  text-white z-50 inset-shadow-[3px_3px_6px_rgba(255,255,255,0.2),-3px_-3px_6px_rgba(255,255,255,0.2)] items-center rounded-2xl backdrop-blur-xl' style={{ paddingBlock: "1rem" }}>
                 <ul className='flex justify-around '>
-                    {MenuItems.map((item) => (
-
-                        <li key={item.label} className=''><Link href={item.href}>{item.icon}</Link></li>
-                    ))}
+                    {MenuItems.map((item) => {
+                        const Icon = item.iconComponent;
+                        return (
+                            <li key={item.label} className={`${isActive(item.href) ? 'scale-125 transition-all ease-in-out duration-300' : ''}`}>
+                                <Link href={item.href}>
+                                    <Icon color={isActive(item.href) ? '#A855F7' : '#ffffff'} />
+                                </Link>
+                            </li>
+                        )
+                    })}
                 </ul>
             </nav>
         </>
